b4b41e2145949c59beba92f5d82db48af32c7a12
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-22 16:21:24 +0200

    Fix overflow issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,6 @@ export default function Home() {
                 width={ 120 }
                 height={ 180 }
                 className="book-image"
-                objectFit="cover"
               />
             </figure>
             <div className="book-info">
